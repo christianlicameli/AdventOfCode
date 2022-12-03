cfb12d9756bd37a2fb272e5572cb921998a96a0a
Christian <28650985+christianlicameli@users.noreply.github.com>
2022-12-03 16:24:09 +0100

    first commit with solution, library and the first problem solved

---
+++ AOC2022lib/Day01.cs
@@ -0,0 +1,32 @@
+﻿namespace AOC2022lib
+{
+    public class Day01
+    {
+
+        public string A()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"InputFiles\D01A.txt");
+
+            long maxCalories = 0;
+            long currentCalories = 0;
+
+            foreach (string line in lines)
+            {
+                if (string.IsNullOrEmpty(line.Trim()))
+                {
+                    //check numbers and go to next elf
+                    if (currentCalories > maxCalories) maxCalories = currentCalories;
+                    currentCalories = 0;
+                }
+                else
+                {
+                    currentCalories = currentCalories + long.Parse(line.Trim());
+                }
+            }
+
+            return maxCalories.ToString();
+        }
+
+        
+    }
+}
---
+++ AOC_Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using AOC2022lib;
+using System;
+
+namespace AOC_Console  
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Advent of Code!");
+
+            AOC2022lib.Day01 aoc2022d1 = new AOC2022lib.Day01();
+            Console.WriteLine("2022 Day 01:  {0}", aoc2022d1.A());
+
+
+            
+            Console.ReadLine();
+        }
+    }
+}
