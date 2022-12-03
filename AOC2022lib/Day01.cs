44b8d179862da6cada356b37d047333603eb1d61
Christian <28650985+christianlicameli@users.noreply.github.com>
2022-12-03 16:32:28 +0100

    2022 Day1 part B

--- AOC2022lib/Day01.cs
+++ AOC2022lib/Day01.cs
@@ -27,6 +27,35 @@
             return maxCalories.ToString();
         }
 
-        
+        private class Elf
+        {
+            public string name { get; set; }
+            public long calories { get; set; }
+        }
+        public string B()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"InputFiles\D01A.txt");
+
+            List<Elf> elfList = new List<Elf>();
+
+            int elfIndex = 0;
+            long currentCalories = 0;
+
+            foreach (string line in lines)
+            {
+                if (string.IsNullOrEmpty(line.Trim()))
+                {
+                    //check numbers and go to next elf
+                    elfList.Add(new Elf() { name = $"Elf {++elfIndex}", calories = currentCalories });
+                    currentCalories = 0;
+                }
+                else
+                {
+                    currentCalories = currentCalories + long.Parse(line.Trim());
+                }
+            }
+
+            return elfList.OrderByDescending(l => l.calories).Take(3).Sum(e => e.calories).ToString();
+        }
     }
 }
--- AOC_Console/Program.cs
+++ AOC_Console/Program.cs
@@ -10,7 +10,8 @@ namespace AOC_Console
             Console.WriteLine("Hello Advent of Code!");
 
             AOC2022lib.Day01 aoc2022d1 = new AOC2022lib.Day01();
-            Console.WriteLine("2022 Day 01:  {0}", aoc2022d1.A());
+            //Console.WriteLine("2022 Day 01 A:  {0}", aoc2022d1.A());
+            Console.WriteLine("2022 Day 01 B:  {0}", aoc2022d1.B());
 
 
             
